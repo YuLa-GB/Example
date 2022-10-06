1614cc87d97e9bb263fea115fe86dd395d43c3a2
Yuladenkova <yuliyaladenkova@yandex.ru>
2022-10-07 00:44:32 +0300

    Lesson001_Homework

--- Lesson001_Homework/Program.cs
+++ Lesson001_Homework/Program.cs
@@ -1,1 +1,37 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    Console.WriteLine("Max number is " + n1);
+    Console.WriteLine("Min number is " + n2);
+}
+else
+{
+    Console.WriteLine("Max number is " + n2);
+    Console.WriteLine("Min number is " + n1);
+}
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
