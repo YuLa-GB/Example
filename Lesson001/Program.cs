24cd5a77c5f5a09110b18d873b9b8df7d0f06231
Yuladenkova <yuliyaladenkova@yandex.ru>
2022-10-06 23:30:53 +0300

    Задача 3

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -26,7 +26,7 @@ else
 */
 
 //Задача 2. Напишите программу, которая на вход принимает одно число N, а на выход показывает все целые числа в промежутке от -N до N.
-
+/*
 Console.Write("Input a number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -42,4 +42,26 @@ while(current <= n)
 {
     Console.Write(current + " ");
     current++;
+}
+*/
+
+//  Целочисленное число
+/*
+3162 % 10 -> 2
+3162 % 100 -> 62
+3162 % 1000 -> 162
+*/
+
+// Задача 3. Напишите программу, которая на вход принимает  трехзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.Write("Input a tree-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    Console.WriteLine(n % 10);
+}
+else
+{
+    Console.WriteLine("Uncorrect input!");
 }
