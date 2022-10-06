31aa05063fc1a34f6fa732b94c3d77d7c8ce89f9
Yuladenkova <yuliyaladenkova@yandex.ru>
2022-10-06 22:41:39 +0300

    Задача 1

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
+*/
+
+// Задача 1. Напишите программу, которая на вход принимается  два числа и проверяет является ли первое число квадратом второго
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int q2 = n2 * n2;
+
+if(n1 == q2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
