7b980e3c74dd8fda010a51858d0adc82b7943af1
Yuladenkova <yuliyaladenkova@yandex.ru>
2022-10-06 23:10:03 +0300

    Задача 2

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Your number is " + num);
 */
 
 // Задача 1. Напишите программу, которая на вход принимается  два числа и проверяет является ли первое число квадратом второго
-
+/*
 Console.Write("Input a first number: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -22,4 +22,24 @@ if(n1 == q2)
 else
 {
     Console.WriteLine("No");
+}
+*/
+
+//Задача 2. Напишите программу, которая на вход принимает одно число N, а на выход показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+if(n < 0)
+{
+    current = n;
+    n = -n;
+}
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
 }
