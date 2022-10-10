19f32dfc0787fb046354bc6f74705712ba778538
YuLa-GB <yuliyaladenkova@yandex.ru>
2022-10-10 23:10:50 +0300

    Задача 1

---
+++ Lesson002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1. Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
+int FindBiggerDigit(int num)
+{
+    int result;                   // когда метод что-то возвращает, например int, то команда обязательно должна заканчиваться return
+    
+    if(num < 10 || num >-100)      // проверили, что число не является двузначным, то вернули 0
+        return 0;
+    else                            
+    {
+        int ed = num % 10;         // либо мы расчитываем, получив цифры, какая из них больше и выводим ту самую цифру
+        int dec = num / 10;
+
+        if(ed > dec)
+        {
+            result = ed;
+        }
+        else
+        {
+            result = dec;
+        }
+    }
+
+    return result;
+}
+
+Console.Write("Input a two-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int biggerDigit = FindBiggerDigit(number);
+
+Console.WriteLine($"Bigger digit of {number} is {biggerDigit}");
