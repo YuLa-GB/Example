077fbcbff229dbd33db554a8d46266cc022b1f4f
YuLa-GB <yuliyaladenkova@yandex.ru>
2022-10-16 18:11:38 +0300

    Задача 13

--- Lesson002_Homework/Program.cs
+++ Lesson002_Homework/Program.cs
@@ -1,12 +1,34 @@
 ﻿// Задача 10. Напишите программу, которая на вход  принимает трехзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int ShowTheSecond (int num)
 {
     int ed = num / 10;
     int result = ed % 10;
-    return result;
+    return result; 
 }
 
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(ShowTheSecond(number));
+*/
+
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+string ShowTheThird (int num)
+{
+    int length = Convert.ToString(num).Length;
+
+       if(length > 2)
+       {
+       // Console.WriteLine("TEST " + (num / Math.Pow(10, length - 3)));
+        int result = num / Convert.ToInt32(Math.Pow(10, length - 3)) % 10;
+        return Convert.ToString(result);
+       }
+
+     return "Третьей цифры нет";
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ShowTheThird(number));
+*/
