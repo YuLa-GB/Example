20b598bbc42072af2e095fe0c8eadb4802c5d533
YuLa-GB <yuliyaladenkova@yandex.ru>
2022-10-16 16:46:31 +0300

    Задача 10

--- Lesson002_Homework/Program.cs
+++ Lesson002_Homework/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" Hello, World!");
+﻿// Задача 10. Напишите программу, которая на вход  принимает трехзначное число и на выходе показывает вторую цифру этого числа.
+
+int ShowTheSecond (int num)
+{
+    int ed = num / 10;
+    int result = ed % 10;
+    return result;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ShowTheSecond(number));
